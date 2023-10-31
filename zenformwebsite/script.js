6b84b46f6c4bb94752ea9117504677a808582044
tanuj1203 <145794048+tanuj1203@users.noreply.github.com>
2023-10-31 23:16:44 +0530

    Add files via upload

---
+++ zenformwebsite/script.js
@@ -0,0 +1,8 @@
+// Replace with your actual Google Form URL
+const googleFormUrl = "https://forms.gle/SJuGFrGoRDvuXifa6"
+document.querySelector(".form-container").innerHTML = `
+<iframe src="${googleFormUrl}" width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+`;
+document.querySelector(".form-container").innerHTML = `
+<iframe src="${googleFormUrl}" width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+`;
